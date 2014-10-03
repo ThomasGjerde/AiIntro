5303f70382c17c3c1b17cab37b6888b00ab09520
magnulef <lefdal.magnus@gmail.com>
2014-10-03 17:52:53 +0200

    gridDjikstraSearch

---
+++ src/navigation/GridDjikstraSearch.java
@@ -0,0 +1,49 @@
+package navigation;
+
+import gui.BoardGraphics;
+import model.Node;
+import model.Node.Status;
+import model.Point;
+import search.DjikstraSearch;
+
+
+public class GridDjikstraSearch extends DjikstraSearch{
+	Board board;
+	BoardGraphics graphics;
+	public GridDjikstraSearch(Node startNode, Point endPoint, Board board) {
+		super(startNode, endPoint);
+		this.board = board;
+		graphics = new BoardGraphics(board);
+	}
+
+	@Override
+	protected void updateGui() {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//System.out.println("GUI");
+		for(int i = 0; i < board.size.x; i++){
+			for(int j = 0; j < board.size.y; j++){
+				if(board.boardArray[i][j].status == Status.Visiting){
+					board.boardArray[i][j].status = Status.Visited;
+				}
+			}
+		}			
+		Node r2 = currentNode;
+		while(r2.parent != null)
+		{
+			r2.setStatus(Status.Visiting);
+			r2 = r2.parent;
+		}
+		graphics.setBoard(board);
+	}
+
+	@Override
+	protected void processCurrentNode() {
+		
+	}
+
+}
